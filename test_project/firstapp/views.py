fccd61c4540ab054fd8830922abcbb23c69686af
zyuzinyu64 <54856910+zyuzinyu64@users.noreply.github.com>
2021-09-17 16:28:07 +0300

    Update views.py

--- test_project/firstapp/views.py
+++ test_project/firstapp/views.py
@@ -1,9 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Person
- 
-# def index(request):
-#     return HttpResponse("<h2>Главная</h2>")
 
 def index(request):
     return render(request, "index.html")
